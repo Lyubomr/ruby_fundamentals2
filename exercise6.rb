7604e3ffdf13958c8517806043410652479886a3
Lyubomr <s.lyubomyr@gmail.com>
2016-01-13 22:56:39 -0500

    Created list printing method

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,12 @@
+#1 Created list then added rice
 grocery_list = ["*bananas", "*eggs", "*milk"]
   puts grocery_list
 grocery_list << "*rice"
   puts grocery_list
+
+# 2 Method to print list
+  def print_list(list)
+    list.each do |item|
+      puts "* #{item}"
+  end
+end
