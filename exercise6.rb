cfa1cc73b814ec111aa674ecc38257e62d68de23
Lyubomr <s.lyubomyr@gmail.com>
2016-01-13 23:01:26 -0500

    Removed asterisks from list, moved the print_list method to the top

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,14 @@
-#1 Created list then added rice
-grocery_list = ["*bananas", "*eggs", "*milk"]
-  puts grocery_list
-grocery_list << "*rice"
-  puts grocery_list
 
-# 2 Method to print list
+
+# Method to print list
   def print_list(list)
     list.each do |item|
       puts "* #{item}"
   end
 end
+
+grocery_list = ["bananas", "eggs", "milk"]
+  print_list(grocery_list)
+
+grocery_list << "rice"
+print_list(grocery_list)
