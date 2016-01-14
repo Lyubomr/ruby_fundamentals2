abd35034833b8721349cc0da068e5bb73b4abeab
Lyubomr <s.lyubomyr@gmail.com>
2016-01-13 23:06:41 -0500

    Added list count

--- exercise6.rb
+++ exercise6.rb
@@ -7,8 +7,11 @@
   end
 end
 
+# Print out grocery list
 grocery_list = ["bananas", "eggs", "milk"]
   print_list(grocery_list)
-
+# Added rice to list and printed list again.
 grocery_list << "rice"
 print_list(grocery_list)
+# Count the list.
+puts "There are #{grocery_list.count} on the list"
