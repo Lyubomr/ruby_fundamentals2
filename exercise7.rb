7fad373bf04233d4212ad276f3b36804c5857d75
Lyubomr <s.lyubomyr@gmail.com>
2016-01-14 10:04:51 -0500

    Added method to display student list

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,15 @@
+# Number of students
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+# Method to display students
+def display_hash(students)
+  students.each do |cohort, number|
+    puts "#{cohort}: #{number}"
+  end
+end
+
+display_hash(students)
