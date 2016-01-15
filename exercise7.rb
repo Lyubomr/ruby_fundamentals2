ea8bbf4a63ae4b51a86f0a75573e62cf3d4e9ca9
Lyubomr <s.lyubomyr@gmail.com>
2016-01-15 14:21:03 -0500

    Added size change and deleted 2nd cohort

--- exercise7.rb
+++ exercise7.rb
@@ -13,3 +13,30 @@ def display_hash(students)
 end
 
 display_hash(students)
+
+students[:cohort4] = 43
+
+display_hash(students)
+
+# Print cohort names
+def display_cohort(cohort)
+  cohort.each do  |cohort_name, number|
+    puts cohort_name
+  end
+end
+
+display_cohort(students)
+
+# method to change size
+def size_change(hash_to_be_changed, size_input)
+  hash_to_be_changed.each do |cohort, value|
+    hash_to_be_changed[cohort] = (value * size_input).to_i
+  end
+end
+
+size_change(students, 1.05)
+display_hash(students)
+
+students.delete(:cohort2)
+
+display_hash(students)
